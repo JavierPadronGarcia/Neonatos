a946b2049608f4b7db7cdfebd00486df9e52f2fd
JavierPadronGarcia <padrongarciajavier04@gmail.com>
2023-11-21 18:19:20 +0000

    finished issue#17 - all request need auth token

--- backend/routes/case.routes.js
+++ backend/routes/case.routes.js
@@ -1,22 +1,23 @@
 module.exports = app => {
   const cases = require("../controllers/case.controller");
-
+  const auth = require('../controllers/auth');
+  
   var router = require("express").Router();
 
   //create a new case
-  router.post("/", cases.create);
+  router.post("/", auth.isAuthenticated, cases.create);
 
   //retrieve all cases
-  router.get("/", cases.findAll);
+  router.get("/", auth.isAuthenticated, cases.findAll);
 
   //retrieve a single case by id
-  router.get("/:id", cases.findOne);
+  router.get("/:id", auth.isAuthenticated, cases.findOne);
 
   //update a case with given id
-  router.put("/:id", cases.update);
+  router.put("/:id", auth.isAuthenticated, cases.update);
 
   //delete a case with given id
-  router.delete("/:id", cases.delete);
+  router.delete("/:id", auth.isAuthenticated, cases.delete);
 
-  app.use('/api/cases', router);
+  app.use('/api/cases', auth.isAuthenticated, router);
 };
--- backend/routes/groupenrolement.routes.js
+++ backend/routes/groupenrolement.routes.js
@@ -1,31 +1,32 @@
 module.exports = app => {
-  const groupEnrolement = require("../controllers/groupenrolement.controller")
+  const groupEnrolement = require("../controllers/groupenrolement.controller");
+  const auth = require('../controllers/auth');
 
   var router = require("express").Router();
 
   // assign students to groups or groups to students
-  router.post("/", groupEnrolement.create);
+  router.post("/", auth.isAuthenticated, groupEnrolement.create);
 
   // retrieve all data from groupenrolements
-  router.get("/", groupEnrolement.findAll);
+  router.get("/", auth.isAuthenticated, groupEnrolement.findAll);
 
   //retrieve all students in a group
-  router.get("/group/:id", groupEnrolement.findAllStudentsInGroup);
+  router.get("/group/:id", auth.isAuthenticated, groupEnrolement.findAllStudentsInGroup);
 
   //retrieve the count of the students in a group
-  router.get("/studentcount/group/:id", groupEnrolement.getCountOfStudentsInGroup);
+  router.get("/studentcount/group/:id", auth.isAuthenticated, groupEnrolement.getCountOfStudentsInGroup);
 
   //retrieve all students ordered by group desc
-  router.get("/orderdesc", groupEnrolement.findAllOrderedByGroupDesc);
+  router.get("/orderdesc", auth.isAuthenticated, groupEnrolement.findAllOrderedByGroupDesc);
 
   //retrieve all students not in this table
-  router.get("/studentsnotinagroup", groupEnrolement.findAllStudentsNotInAGroup);
+  router.get("/studentsnotinagroup", auth.isAuthenticated, groupEnrolement.findAllStudentsNotInAGroup);
 
   //update
-  router.put("/:id", groupEnrolement.update);
+  router.put("/:id", auth.isAuthenticated, groupEnrolement.update);
 
   //delete
-  router.delete("/:id", groupEnrolement.remove);
+  router.delete("/:id", auth.isAuthenticated, groupEnrolement.remove);
 
   app.use('/api/groupenrolement', router);
 }
